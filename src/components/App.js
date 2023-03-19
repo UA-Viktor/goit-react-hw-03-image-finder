aaa3505c1f1844ec283160b201c997b3f07e3079
UA-Viktor <v.korenkov@icloud.com>
2023-03-19 19:32:55 +0200

    go_hw_3-2

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Container } from './App.styled';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Container>Hello</Container>
+      </>
+    );
+  }
+}
+
+export default App;
